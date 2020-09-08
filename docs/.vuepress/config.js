bdff6889de8615c5c8629d31cb6b770d04380afd
子恒 <libin@zihengdeMacBook-Pro.local>
2020-09-09 01:47:44 +0800

    blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: "子恒的博客",
+  title: "子恒的前端精要",
   description: 'web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github等技术文章。', // 描述,以 <meta> 标签渲染到页面html中
   // base: '/', // '/<github仓库名>/'， 默认'/'
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
--- docs/index.md
+++ docs/index.md
@@ -1,7 +1,7 @@
 ---
 home: true
 # heroImage: /img/web.png
-heroText: 子恒的博客
+heroText: 子恒的前端精要
 tagline: 道阻且长，行则将至
 # actionText: 立刻进入 →
 # actionLink: /web/
